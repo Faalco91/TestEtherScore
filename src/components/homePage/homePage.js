6da9146d5f7837912ad071734e0edef920814c4d
Ouail Amarir <ouailamarir@gmail.com>
2024-10-19 00:21:35 +0200

    Installer les dépendances ethers et i18next; structurer le projet; mettre en place la connexion réussie à MetaMask.

---
+++ src/components/homePage/homePage.js
@@ -0,0 +1,9 @@
+import WalletConnection from "../walletConnect/walletConnect"
+
+export default function HomePage(){
+    return (
+        <div>
+            <WalletConnection />
+        </div>
+    )
+}
---
+++ src/components/walletConnect/walletConnect.js
@@ -0,0 +1,32 @@
+'use client'
+import React, { useState } from 'react';
+import { metamaskConnect } from '../metamaskConnect/metamaskConnect';
+import styles from './walletConnect.module.css'
+
+const WalletConnection = () => {
+
+  const [walletAddress, setWalletAddress] = useState(null); //Utilisation de useState pour stocker l'adresse du portefeuille
+  const handleConnectWallet = async () => {
+    const address = await metamaskConnect(); // Appel à la fonction metamaskConnect pour se connecter à MetaMask
+    if (address) {
+      setWalletAddress(address); // Mise à jour de l'état avec l'adresse du portefeuille une fois connecté
+    }
+  };
+
+  return (
+    <div className={styles.connection}>
+      <h1>Connectez-vous avec Metamask !</h1>
+      {walletAddress ? (
+        <div>
+          <p>Vous êtes bien connecté au wallet {walletAddress}</p>
+        </div>
+      ) : (
+        <button className={styles.btnConnect} onClick={handleConnectWallet}>
+          Connexion au Wallet
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default WalletConnection;
