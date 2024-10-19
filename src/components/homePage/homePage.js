f7a496b4f51dd7582ea75bf3f24c5133c83b2679
Ouail Amarir <ouailamarir@gmail.com>
2024-10-19 17:56:14 +0200

    Avancement du composant homePage affichant le contenu principal, avec option de connexion au wallet

--- src/components/homePage/homePage.js
+++ src/components/homePage/homePage.js
@@ -1,9 +1,25 @@
+import Image from "next/image"
 import WalletConnection from "../walletConnect/walletConnect"
+import styles from "./homePage.module.css"
+import illu1 from '../../../public/images/etherwallet-img.webp'
 
 export default function HomePage(){
     return (
-        <div>
-            <WalletConnection />
-        </div>
+        <section className={styles.mainSection}>
+            <div className={styles.mainContainer}>
+                <article>
+                    <h1>Gérez votre Portefeuille Crypto</h1>
+                    <p>Connectez-vous à MetaMask et prenez le contrôle de votre portefeuille Ethereum en toute simplicité. 
+                        Suivez vos 10 dernières transactions, consultez votre solde, et prochainement, gérez vos actifs de manière sécurisée. 
+                    </p>
+                    <WalletConnection />
+                </article>
+
+                <div className={styles.imageContainer}>
+                    <Image src={illu1} alt="" />
+                </div>
+            </div>
+        </section>
+
     )
 }
