1820e9e934b9b9f897b329836be0f03fe5648361
Ouail Amarir <ouailamarir@gmail.com>
2024-10-19 17:59:04 +0200

    Ajout du composant header à page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+
 import HomePage from "@/components/homePage/homePage";
+import Header from "@/components/header/header";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div>
       <main className={styles.main}>
+        <Header />
         <HomePage />
       </main>
 
