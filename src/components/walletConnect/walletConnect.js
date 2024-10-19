95aed0516ad5206f9a268e3778fe82388f0ea6d6
Ouail Amarir <ouailamarir@gmail.com>
2024-10-19 22:44:37 +0200

    Utilisation du context pour à nouveau se connecter au wallet et obtenir l'adresse de celui-ci. Cela permet de ne pas passer par des props, simplifie le code et améliore la lisibilité

--- src/components/walletConnect/walletConnect.js
+++ src/components/walletConnect/walletConnect.js
@@ -1,17 +1,11 @@
 'use client'
 import React, { useState } from 'react';
-import { metamaskConnect } from '../metamaskConnect/metamaskConnect';
+import { useWallet } from '../contexts/walletContext';
 import styles from './walletConnect.module.css'
 
 const WalletConnection = () => {
 
-  const [walletAddress, setWalletAddress] = useState(null); //Utilisation de useState pour stocker l'adresse du portefeuille
-  const handleConnectWallet = async () => {
-    const address = await metamaskConnect(); // Appel à la fonction metamaskConnect pour se connecter à MetaMask
-    if (address) {
-      setWalletAddress(address); // Mise à jour de l'état avec l'adresse du portefeuille une fois connecté
-    }
-  };
+  const {walletAddress, handleConnectWallet} = useWallet();
 
   return (
     <div className={styles.connection}>
