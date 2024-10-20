e183054ac045e06b99fe4543634731f629127925
Ouail Amarir <ouailamarir@gmail.com>
2024-10-20 17:45:29 +0200

    Importation de tailwindcs

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,11 @@ import styles from "./page.module.css";
 
 import HomePage from "@/components/homePage/homePage";
 import Header from "@/components/header/header";
+import 'tailwindcss/tailwind.css' 
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.page}>
       <main className={styles.main}>
         <Header />
         <HomePage />
