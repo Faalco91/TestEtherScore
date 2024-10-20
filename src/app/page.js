2506cd56eb1f594080eb2bc6c942a2030084d048
Ouail Amarir <ouailamarir@gmail.com>
2024-10-20 20:59:49 +0200

    Suppression de code inutile

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,13 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 
 import HomePage from "@/components/homePage/homePage";
 import Header from "@/components/header/header";
-import 'tailwindcss/tailwind.css' 
+import 'tailwindcss/tailwind.css';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <div>
+      <main>
         <Header />
         <HomePage />
       </main>
