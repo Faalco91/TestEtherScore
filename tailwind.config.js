bae251a253e960719fbcd336c7ac71e6506f35bd
Ouail Amarir <ouailamarir@gmail.com>
2024-10-22 00:49:55 +0200

    Configuration supplémentaire pour ajouter une couleur différente au composant themeSwitcher à l'aide de Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#563a85"
+      }
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
