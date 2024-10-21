dcd80d9bcb644eecc786d38e2a13aff4c28200a2
Ouail Amarir <ouailamarir@gmail.com>
2024-10-22 00:47:13 +0200

    Ajout de la traduction dans le fichier walletConnect.js et suppression de code inutile.

--- src/components/walletConnect/walletConnect.js
+++ src/components/walletConnect/walletConnect.js
@@ -2,12 +2,14 @@
 import React, { useState } from 'react';
 import { useWallet } from '../contexts/walletContext';
 import styles from './walletConnect.module.css'
-
 import WalletModal from '../walletModal/walletModal';
+import { useTranslation } from 'react-i18next';
 
 const WalletConnection = () => {
 
-  const {walletAddress, walletBalance ,handleConnectWallet} = useWallet();
+  const { t } = useTranslation();
+
+  const {walletAddress, handleConnectWallet} = useWallet();
 
     // Crée un état pour gérer l'ouverture/fermeture de la modal
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -26,13 +28,15 @@ const WalletConnection = () => {
     <div className={styles.connection}>
       {walletAddress ? (
         <div>
-          <a className={styles.btnConnect} onClick={handleOpenModal}>Consulter mon Wallet</a>
+          <a className={styles.btnConnect} onClick={handleOpenModal}>{t('cta2')}</a>
           {isModalOpen && <WalletModal isOpen={isModalOpen} onClose={handleCloseModal} />}
         </div>
       ) : (
-        <a className={styles.btnConnect} onClick={handleConnectWallet}>
-          Me connecter à mon wallet
-        </a>
+        <div>
+          <a className={styles.btnConnect} onClick={handleConnectWallet}>
+          {t('cta1')}
+          </a>
+        </div>
       )}
     </div>
   );
