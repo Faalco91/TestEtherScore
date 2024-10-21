f93330722300e2c32641760461f960978a35f7d2
Ouail Amarir <ouailamarir@gmail.com>
2024-10-22 00:37:34 +0200

    Ajout des données récupérées via Etherscan à la modale du wallet, ainsi que l'intégration de i18next pour la traduction du texte en plusieurs langues.

--- src/components/walletModal/walletModal.js
+++ src/components/walletModal/walletModal.js
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button } from "@nextui-org/react";
 import { useWallet } from "../contexts/walletContext";
+import { useTranslation } from 'react-i18next';
 import styles from './walletModal.module.css'
 import ethIcon from '../../../public/images/currency.png'
 
 export default function WalletModal({ isOpen, onClose }) {
 
-  const {walletAddress, walletBalance ,handleConnectWallet} = useWallet();
+  const { t } = useTranslation();
+
+  const {walletAddress, walletBalance, transaction, handleConnectWallet} = useWallet();
 
   return (
     <>
@@ -15,28 +18,41 @@ export default function WalletModal({ isOpen, onClose }) {
       <Modal backdrop="blur" isOpen={isOpen} onClose={onClose}         classNames={{
           backdrop: "bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20"
         }}>
-        <ModalContent style={{ maxWidth: '900px', width: '100%' }}>
+        <ModalContent style={{ maxWidth: '700px', width: '100%' }}>
           <>
-            <ModalHeader className="flex flex-col gap-1 text-center"><h2>Adresse du Wallet : {walletAddress}</h2></ModalHeader>
+            <ModalHeader className="flex flex-col pb-0 gap-1 text-center">
+              <div className={styles.modalHeader}><h2>{t("balance")}</h2></div>
+            </ModalHeader>
             <ModalBody>
               {walletAddress ? (
                 <div> 
                   <div className={styles.ethBalance}>
-                    <p>{walletBalance} ETH.</p>
-                    <div><Image src={ethIcon} width={70} alt='' /></div>
+                    <p>{walletBalance} ETH</p>
+                    <div><Image src={ethIcon} width={40} alt='' /></div>
                   </div>
                 </div>
               ): (
-                <p>Erreur: Veuillez réessayer ulterieurement.</p>
+                <p>{t("error1")}</p>
               )}
 
-              <p>
-                Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit
-                dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. 
-                Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. 
-                Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur 
-                proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
-              </p>
+              <section className={styles.transactionSection}>
+                <h3>{t("transaction")}</h3>
+                {transaction && transaction.length > 0 ? (
+                  <ul>
+                    {transaction.map((transaction) => (
+                      <li key={transaction.hash}>
+                        <p>Hash: {transaction.hash}</p>
+                        <p>Statut: {transaction.status}</p>
+                        <p>Émetteur: {transaction.from}</p>
+                        <p>Destinataire: {transaction.to}</p>
+                        <p>Date: {new Date(transaction.timestamp * 1000).toLocaleString()}</p>
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <p>{t("error2")}</p> // Message à afficher si aucune transaction
+                )}
+              </section>
             </ModalBody>
             <ModalFooter>
               <Button color="danger" variant="light" onPress={onClose}>
