55119be4dd3429ae2d5b42c86ff9dbc0caf07f5a
Ouail Amarir <ouailamarir@gmail.com>
2024-10-22 00:43:22 +0200

    Modification du fichier pour intégrer la traduction avec `i18next` et ajout du composant `LanguageChanger` dans la barre de navigation.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,41 +4,37 @@ import styles from "./header.module.css";
 import React from "react";
 import { useWallet } from "../contexts/walletContext";
 import Switcher from "../themeSwitcher/themeSwitcher";
+import { useTranslation } from "react-i18next";
+import LanguageChanger from "../languageSwitcher/languageSwitcher";
 
 export default function Header() {
 
+    const { t } = useTranslation();
     const { handleConnectWallet, handleDisconnectWallet, walletAddress } = useWallet(); 
     
-
     return (
         <section className={styles.headerSection}>
             <div className={styles.logo}>
                 <a><img width="60" height="60" src="https://img.icons8.com/nolan/64/metamask-logo.png" alt="metamask-logo"/></a>
             </div>
-            <div className={styles.navBarContainer}>
             <nav className={styles.navBar}>
-                <ul>
-                    <li><a>Accueil</a></li>
-                    <li><a>Services</a></li>
-                    <li><a>Contact</a></li>
-                </ul>
             </nav>
             <div className={styles.navBarOptions}>
-                <div className={styles.languageBtn}>
-                    <btn>FR</btn>
-                </div>
                 <div className={styles.connectBtn}>
                     {walletAddress ? (
-                        <a onClick={handleDisconnectWallet}>Déconnexion</a>
+                        <a onClick={handleDisconnectWallet}>{t('disconnection')}</a>
 
                     ):(
-                        <a onClick={handleConnectWallet}>Connexion</a>
+                        <a onClick={handleConnectWallet}>{t('connection')}</a>
                     )}
                 </div>
                 <div>
                     <Switcher />
                 </div>
-            </div>
+                <div className={styles.languageBtn}>
+                    <LanguageChanger />
+                </div>
+
             </div>
         </section>
     )
